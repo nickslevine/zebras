2768e4995abbcb5fce0a07c977c4ce548d23fc42
Nick Levine <nickslevine@gmail.com>
2018-12-24 14:02:20 +0000

    Hello World

---
+++ README.md
@@ -0,0 +1,132 @@
+# zebras - data analysis library for javascript
+
+Zebras is a data manipulation and analysis library written in JavaScript offering the convenience of pandas or R. 
+
+Unlike pandas, zebras is purely functional. All zebras functions accept data as an argument and return a new data structure transformed. 'Dataframes' are simply arrays of JS objects without any special methods. 
+
+Zebras is built on top of [Ramda](https://github.com/Ramda/ramda). Think of zebras as a collection of Ramda functions and compositions thereof curated for common data processing and analysis tasks. 
+
+As in Ramda, all zebras functions are curried, making them easier to chain together. This means that, for example, `zebras.sort(col, df)` is equivalent to `zebras.sort(col)(df)`. 
+
+## Example Usage
+```javascript
+const z = require('zebras')
+const data = z.readCSV('data.csv')
+const parsedData = z.parseNums(['speed','acc'], data)
+const speed = z.getCol('speed', parsedData)
+const avgSpeed = z.mean(speed)
+```
+
+Piping example:
+
+```javascript
+const z = require('zebras')
+const avgSpeed = z.pipe(
+  [           // array of functions for input to travel through
+    z.readCSV,
+    z.parseNums(['speed','acc']),
+    z.getCol('speed'),
+    z.mean
+  ]
+)('data.csv')
+```
+
+## Documentation
+
+In the documentation, `df` signifies an array of Javascript objects, while `arr` is a simple Javascript array of values.
+
+### IO
+
+Read a CSV file:
+`z.readCSV(filepath)`
+
+Write a CSV file:
+`z.toCSV(filepath, df)`
+
+Print dataframe:
+`z.print(df)`
+
+Print first n rows of dataframe:
+`z.head(n, df)`
+
+Print last n rows of dataframe:
+`z.tail(n, df)`
+
+### Manipulating dataframes
+
+Filter rows:
+`z.filter(func, df)`
+
+Sort rows by a column:  
+`z.sortByCol(columnName, direction, df)`
+`direction = 'asc'` for ascending and `direction = 'desc'` for descending
+
+Sort rows using custom sorting function: 
+`z.sort(func, df)`
+
+Convert columns to numerical type:
+`z.parseNums(columnNamesArray, df)`
+
+Convert columns to datestamp: 
+`z.parseDates(columnNamesArray, df)`
+
+Select a subset of columns:
+`z.pickCols(columnNamesArray, df)`
+
+Concatenate two dataframes:
+`z.concat(df1, df2)`
+
+Get rows by index:
+`z.slice(start, end, df)`
+
+Add a new column to a dataframe from an array:
+`z.addCol(columnName, arr, df)`
+
+Create a new array based on columns from existing array:
+`z.deriveCol(func, df)`
+
+### Analyzing series
+
+Extract a series to an array from a dataframe:
+`z.getCol(columnName, df)`
+
+Min of series: 
+`z.min(arr)`
+
+Max of series:
+`z.max(arr)`
+
+Range of series:
+`z.getRange(arr)`
+
+Mean of series:
+`z.mean(arr)`
+
+Median of series:
+`z.median(arr)`
+
+Standard deviation of series:
+`z.std(arr)`
+
+Correlation between two series:
+`z.corr(arr1,arr2)`
+
+Calcualte rolling statistics (works with z.min, z.max, z.mean, z.median, z.std or any user-supplied function that takes an array as a single argument):
+`z.rolling(func, n, arr)`
+
+Get unique values: 
+`z.unique(arr)`
+
+Count number of unique values:
+`z.countUnique(arr)`
+
+Count number of occurences of each value:
+`z.valueCounts(arr)`
+
+Create groupBy object:
+`z.groupBy(func, df)`
+
+### Composition
+
+Pipe functions together:
+`z.pipe([functions], df)`
---
+++ zebras.js
@@ -0,0 +1,261 @@
+const fs = require('fs')
+const R = require('ramda')
+const Table = require('cli-table3')
+
+const readCSV = R.curry((filepath) => {
+  const data = fs.readFileSync(filepath).toString('utf8') 
+  const dataProcessed = R.pipe(
+    R.split('\n'),
+    R.map(R.split(',')),
+    R.reject(x=>x=='')
+  )(data)
+  const headers = R.flatten(R.take(1)(dataProcessed))
+  const rows =  R.tail(dataProcessed)
+  const df = R.map((r) => R.zipObj(headers)(r), rows)
+  return df 
+})
+
+const toCSV = R.curry((filepath, df) => {
+  const headers = R.join(',',R.keys(R.nth(0,df)))
+  const rows = R.map(R.values,df);
+  const rowStrings = R.join('\n',R.map(R.join(','),rows));
+  fs.writeFileSync(filepath,headers+'\n'+rowStrings)
+})
+
+const filter = R.curry((func, df) => {
+  return R.filter(func, df)
+})
+
+const sort = R.curry((func, df) => {
+  return R.sort(func, df)
+})
+
+const sortByCol = R.curry((col, direction, df) => {
+  return R.sort((a,b) => {
+    if (direction == "asc") {
+      return a[col] - b[col];
+    } else {
+      return b[col] - a[col];
+    }
+  }, df)
+})
+
+const parseNums = R.curry((cols, df) => {
+  const convertRow = (r) => {
+    const converter = (value, key, obj) => {
+      if (R.includes(key, cols)){
+        return parseFloat(value)
+      } else {
+        return value;
+      }
+    }
+    return R.mapObjIndexed(converter, r)
+  }
+  return R.map(convertRow, df)
+})
+
+const parseDates = R.curry((cols, df) => {
+  const convertRow = (r) => {
+    const converter = (value, key, obj) => {
+      if (R.includes(key, cols)){
+        return Date.parse(value)
+      } else {
+        return value;
+      }
+    }
+    return R.mapObjIndexed(converter, r)
+  }
+  return R.map(convertRow, df)
+})
+
+const pickCols = R.curry((cols, df) => {
+  return R.pick(cols, df)
+})
+
+const getCol = R.curry((col, df) => {
+  return R.map(R.prop(col), df)
+})
+
+const mean = R.curry((arr) => {
+  const filteredArr = R.reject(isNaN, arr)
+  return R.mean(filteredArr)
+})
+
+const median = R.curry((arr) => {
+  const filteredArr = R.reject(isNaN, arr)
+  return R.median(filteredArr)
+})
+
+
+const std = R.curry((arr) => {
+  const filteredArr = R.reject(isNaN, arr)
+  const sampleMean = R.mean(filteredArr);
+  const n = R.length(filteredArr) 
+  const diffs = R.map(x=>x-sampleMean, filteredArr)
+  const diffsSquared = R.map(x=>Math.pow(x,2),diffs)
+  const summed = R.sum(diffsSquared)
+  return Math.sqrt(R.divide(summed, R.subtract(n,1)))
+})
+
+const corr = R.curry((arr1,arr2) => {
+  if (R.length(arr1) != R.length(arr2)) {
+    return "Arrays are not the same length";
+  } else {
+    const sampleMean1 = R.mean(arr1);
+    const sampleMean2 = R.mean(arr2);
+    const std1 = std(arr1);
+    const std2 = std(arr2);
+    const nMinusOne = R.subtract(R.length(arr1),1);
+    const rangeArray = R.range(0, R.length(arr1));
+    const products = R.map((x) => {
+      return (arr1[x] - sampleMean1) * (arr2[x] - sampleMean2);
+    }, rangeArray); 
+    const summedProducts = R.sum(products);
+    return summedProducts / (nMinusOne * std1 * std2)
+  }
+})
+
+
+const pipe = R.curry((funcs, df) => {
+  return R.pipe(
+    ...funcs
+  )(df)
+})
+
+const concat = R.curry((df1,df2)=> {
+  return R.concat(df1,df2)
+})
+
+const groupBy = R.curry((func, df) => {
+  return R.groupBy(func, df)
+})
+
+const slice = R.curry((start, end, df) => {
+  return R.slice(start, end, df)
+})
+
+const unique = R.curry((arr)=> {
+  return R.uniq(arr)
+})
+
+const max = R.curry((arr) => {
+  const filteredArr = R.reject(isNaN, arr)
+  return R.apply(Math.max, filteredArr) 
+})
+
+const min = R.curry((arr) => {
+  const filteredArr = R.reject(isNaN, arr)
+  return R.apply(Math.min, filteredArr) 
+})
+
+const getRange = R.curry((arr) => {
+  return [min(arr), max(arr)];
+})
+
+const countUnique = R.curry((arr) => {
+  return R.length(R.uniq(arr))
+})
+
+const dropCol = R.curry((col, df) => {
+  return R.map(R.dissoc(col),df)
+})
+
+const valueCounts = R.curry((arr) => {
+  return R.countBy(R.identity, arr)
+})
+
+const addCol = R.curry((col, arr, df) => {
+  if (R.equals(R.length(df), R.length(arr))){
+    return df.map((row, i) => R.assoc(col, arr[i], row))
+  } else {
+    return "Arrays are not of equal length"
+  }
+})
+
+const deriveCol = R.curry((func, df) => {
+  return R.map(func, df)
+})
+
+const print = R.curry((df) => {
+  const headers = R.keys(df[0]);
+  const rows = R.map(R.values, df);
+  let printTable =  new Table({
+    head: headers
+  })
+  printTable.push(
+   ...rows 
+  );
+  console.log(printTable.toString())
+  return ""
+})
+
+const head = (n, df) => {
+  const truncated = R.take(n, df);
+  console.log(print(truncated));
+  return "";
+}
+
+const tail = (n, df) => {
+  const truncated = R.takeLast(n, df);
+  console.log(print(truncated));
+  return "";
+}
+
+const pctChange = R.curry((arr) => {
+  const iRange = R.range(0, arr.length)
+  const result = R.map((i) => {
+    if (i==0) {
+      return NaN
+    } else {
+      return (arr[i] / arr[i-1]) - 1
+    }
+  }, iRange)
+  return result 
+})
+
+const rolling = (func, n, arr) => {
+  const iRange = R.range(0, arr.length);
+  const result = R.map((i) => {
+    if (i + 1 < n) {
+      return 'NotANumber' 
+    } else {
+      const truncated = R.slice(i-n+1, i+1, arr)
+      return func(truncated)
+    }
+  }, iRange)
+  return result;
+}
+
+module.exports = {
+  readCSV: readCSV,
+  toCSV: toCSV,
+  filter: filter,
+  parseNums: parseNums,
+  pickCols: pickCols,
+  getCol: getCol,
+  mean: mean,
+  median: median,
+  std: std,
+  pipe: pipe,
+  concat: concat,
+  groupBy: groupBy,
+  slice: slice,
+  unique: unique,
+  countUnique: countUnique,
+  corr: corr,
+  min: min,
+  max: max,
+  getRange: getRange,
+  dropCol: dropCol,
+  valueCounts: valueCounts,
+  addCol: addCol,
+  deriveCol: deriveCol,
+  print: print,
+  head: head,
+  tail: tail,
+  pctChange: pctChange,
+  rolling: rolling,
+  parseDates: parseDates,
+  sort: sort,
+  sortByCol: sortByCol
+}
