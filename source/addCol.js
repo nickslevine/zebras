bd11d42e379a7a2c2031725606b1574562155481
Nick Levine <nickslevine@gmail.com>
2021-02-22 18:08:14 +0000

    Specify index.d.ts

--- source/addCol.js
+++ source/addCol.js
@@ -25,13 +25,4 @@ const addCol = curry((col, arr, df) => {
   return df.map((row, i) => assoc(col, arr[i], row))
 })
 
-const sransiansiRegex = () => {
-  const pattern = [
-    "[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:[a-zA-Z\\d]*(?:;[a-zA-Z\\d]*)*)?\\u0007)",
-    "(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PRZcf-ntqry=><~]))",
-  ].join("|")
-
-  return new RegExp(pattern, "g")
-}
-
 export default addCol
